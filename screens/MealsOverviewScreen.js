33e789c58c3600e9977122693c6bec87628761d9
Krishna Kant <krishnakant@Krishnas-MacBook-Air.local>
2024-02-28 20:09:46 +0530

    Using React navigation in project

---
+++ screens/MealsOverviewScreen.js
@@ -0,0 +1,24 @@
+import { Text, View, StyleSheet } from 'react-native';
+
+import { MEALS } from '../data/dummy-data';
+
+const MealsOverviewScreen = ({ route }) => {
+  const categoryId = route.params.categoryId;
+
+  return (
+    <View style={styles.container}>
+      <Text>MealsOverviewScreen {categoryId}</Text>
+    </View>
+  );
+};
+
+export default MealsOverviewScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 16,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
